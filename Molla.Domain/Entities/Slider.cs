71d8e5a39d2e29d9dbf4ee692bf76e4ac07c22cf
Lenovo <Lenovo@DESKTOP-588QI00>
2023-11-12 00:02:11 +0330

    add entities

---
+++ Molla.Domain/Entities/Slider.cs
@@ -0,0 +1,18 @@
+﻿using Molla.Domain.Common;
+using Molla.Domain.Enums;
+
+namespace Molla.Domain.Entities
+{
+    public class Slider:BaseEntity
+    {
+        public required string ImageSource { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public string Tag { get; set; }
+        public required string Link { get; set; }
+
+    }
+}
