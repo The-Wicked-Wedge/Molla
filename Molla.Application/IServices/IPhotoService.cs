42469159a9375bc9200c9061e2fbe14b0dc43677
Mehregan Abdi <mehreganabdix@gmail.com>
2023-11-18 19:30:49 +0330

    Set Up And Service Added

---
+++ Molla.Application/IServices/IPhotoService.cs
@@ -0,0 +1,13 @@
+﻿using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+ 
+
+namespace Molla.Application.IServices
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicId);
+
+    }
+}
