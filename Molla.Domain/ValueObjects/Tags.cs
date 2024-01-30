727a2cc110ac6a0858d504bf96c80708ca1f61b3
Lenovo <Lenovo@DESKTOP-588QI00>
2024-01-30 12:21:10 +0330

    debug razor pages

--- Molla.Domain/ValueObjects/Tags.cs
+++ Molla.Domain/ValueObjects/Tags.cs
@@ -6,15 +6,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Molla.Domain.ValueObjects
+namespace Molla.Domain.ValueObjects;
+
+public class Tags : BaseValueObject
 {
+    public string Tag1 { get; set; }
+    public string Tag2 { get; set; }
+    public string Tag3 { get; set; }
+    public string Tag4 { get; set; }
 
-    public class Tags:BaseValueObject
-    {
-        public string Tag1 { get; set; }
-        public string Tag2 { get; set; }
-        public string Tag3 { get; set; }
-        public string Tag4 { get; set; }
-
-    }
 }
+
--- Molla.Presentation/Program.cs
+++ Molla.Presentation/Program.cs
@@ -21,7 +21,7 @@ namespace Molla.Presentation
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
             
 
 
