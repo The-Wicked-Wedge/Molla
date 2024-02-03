6dcafdef64a457323ed4b490186f31979ef6e04c
Mehregan Abdi <mehreganabdix@gmail.com>
2024-02-03 20:34:49 +0330

    OFFTicket Model Added

---
+++ Molla.Domain/IRepositories/IOFFRepository.cs
@@ -0,0 +1,19 @@
+﻿using Molla.Domain.Entities;
+using Molla.Domain.Entities.AdminDashBoard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molla.Domain.IRepositories
+{
+    public interface IOFFRepository
+    {
+        Task<IEnumerable<OFFModel>> GetAllAsync();
+        Task<bool> Create(OFFModel model);
+        bool Delete(OFFModel model);
+
+        Task<IEnumerable<OFFModel>> GetAllTicketsForUser(string Id);
+    }
+}
---
+++ Molla.Infrastructure.persistence/Repositories/OFFRepository.cs
@@ -0,0 +1,21 @@
+﻿using Molla.Domain.Entities;
+using Molla.Domain.Entities.AdminDashBoard;
+using Molla.Domain.IRepositories;
+using Molla.Infrastructure.persistence.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molla.Infrastructure.persistence.Repositories
+{
+    public class OFFRepository(ApplicationDbContext context) : GenericeRepository<OFFModel>(context), IOFFRepository
+    {
+        public async Task<IEnumerable<OFFModel>> GetAllTicketsForUser(string Id)
+        {
+            var allTickets = await GetAllAsync();
+            return allTickets.Where(t => t.UserId == Id).ToList();
+        }
+    }
+}
