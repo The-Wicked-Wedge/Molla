e90b9649e3aa5ee2492c18339df01d9f828e5e83
Abolfazl_Es <airmp13@yahoo.com>
2024-02-04 00:57:56 +0330

    Add AboutUs and ContactUs Entities-Repositories

---
+++ Molla.Domain/IRepositories/IContactUsRepository.cs
@@ -0,0 +1,21 @@
+﻿using Molla.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molla.Domain.IRepositories
+{
+    public interface IContactUsRepository
+    {
+        Task<ContactUs> GetByIdAsync(Guid id);
+
+        #region Generice Repository
+        Task<IEnumerable<ContactUs>> GetAllAsync();
+        Task<bool> Create(ContactUs baner);
+        bool Update(ContactUs baner);
+        bool Delete(ContactUs model);
+        #endregion
+    }
+}
